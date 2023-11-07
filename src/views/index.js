abff98f30a417bbce248fbebde5650f627a44703
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2023-11-06 22:18:23 -0300

    vercel otro

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,5 @@
-import { Home } from '../components/home/Home.jsx';
+// import { Home } from '../components/home/Home.jsx';
 import { About } from '../components/About/About';
 import { Trabajos } from '../components/Trabajos/Trabajos';
 
-export {Home, About, Trabajos}
+export { About, Trabajos}
