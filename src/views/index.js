6a8e9adf6cd5f5795e6e60585150675e4ded4872
David-Rodriguez-1 <ezequiel-07@hotmail.com>
2023-11-06 21:41:32 -0300

    commit

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,5 @@
 import { Home } from '../components/home/Home';
 import { About } from '../components/About/About';
-import { Experience } from '../components/Experience/Experience';
 import { Trabajos } from '../components/Trabajos/Trabajos';
 
-export {Home, About, Experience, Trabajos}
+export {Home, About, Trabajos}
